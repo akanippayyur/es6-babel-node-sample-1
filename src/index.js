09adb9bd596990ba6094d6766c1614ff10413f86
Anoop Kanippayyur <Anoop.Kanippayyur@gmail.com>
2016-09-24 17:17:16 -0700

    Adding the express module and security handlers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
-http.createServer((req, res) => {
-	res.writeHead(200, {'Content-Type': 'text-plain'});
-	res.end('Hello World\n');
-}).listen(30000, '127.0.0.1');
+// Initialize the express app.
+const app = express()
 
-console.log('Server running at http://127.0.0.1:3000/');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Enable helmet to secure the application.
+app.use(helmet());
+app.use(helmet.noCache())
+app.use(helmet.frameguard())
+
+app.get('/', (req, res) => {
+	res.writeHead(200, {'Content-Type': 'text-plain'});	
+	res.end('Hello World!');
+});
+
+http.createServer(app).listen(3000, '127.0.0.1', () => {
+	console.log('Server running at http://127.0.0.1:3000/');
+});
